b0277081e60371e9c69ef515d5e31a9a8400e984
zippon <zippon.wu@gmail.com>
2012-08-20 20:06:23 +0900

    Initial Commit

---
+++ wu_collage_professional/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  wu_collage_professional
+//
+//  Created by Zhipeng Wu on 8/20/12.
+//  Copyright (c) 2012 Zhipeng Wu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
